e32e591f07e89fef79dc080087b83e97b1c76852
otecpsgitpause <34405402+otecpsgitpause@users.noreply.github.com>
2018-03-24 20:14:26 -0300

    mas cambios
    mas cambios

--- controller/conector-controller/conector-controller.js
+++ controller/conector-controller/conector-controller.js
@@ -14,10 +14,10 @@ var conector = {
             console.log(jData);
             if (sistem.sistem1.domain == jData.a) {
                 request.post('https://aadm41ad8cdp90b2m04aepi98p---.herokuapp.com/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ/eyJ1c2VybmFtZSI6InRlc3QiLCJlbWFpbCI6InRlc3QiLCJpYXQiOjE1MDE4NzM5NDMsImV4cCI6MTUwMTg4MDk0M3', { form: { data: req.body.monkey } }, (err, httpResponse, body) => {
-                    console.log('estoy adentro de esto');
+                    console.log('estoy adentro de if');
                     console.log({sistemConfig1:sistem.sistem1});
                     console.log({sistemConfig2:sistemConf.sistem2});
-                    console.log({respuestaApiRest:body});
+                    console.log({respuestaApiRest:body,error:err,httpResponse:httpResponse});
                     cryptoUtil.cryptoMethod.encode(body).then(encode => {
 
                         res.json({
@@ -31,7 +31,10 @@ var conector = {
                 //sistemConf.sistem2.urlBase + sistemConf.sistem2.tkm
             } else if (sistem.sistem3.domain == jData.a) {
                 request.post('https://aadm41ad8cdp90b2m04aepi98p---.herokuapp.com/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ/eyJ1c2VybmFtZSI6InRlc3QiLCJlbWFpbCI6InRlc3QiLCJpYXQiOjE1MDE4NzM5NDMsImV4cCI6MTUwMTg4MDk0M3', { form: { data: req.body.monkey } }, (err, httpResponse, body) => {
-                    console.log('estoy adentro de esto');
+                    console.log('estoy adentro de else if');
+                    console.log({sistemConfig1:sistem.sistem1});
+                    console.log({sistemConfig2:sistemConf.sistem2});
+                    console.log({respuestaApiRest:body,error:err,httpResponse:httpResponse});
                     cryptoUtil.cryptoMethod.encode(body).then(encode => {
 
                         res.json({
